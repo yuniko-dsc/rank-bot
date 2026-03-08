fe930da3a3f210d06c83367b328aa304559aa05c
Yuniko <002notsans+1@gmail.com>
2026-03-08 17:04:15 +0100

    Add files via upload

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -11,27 +11,12 @@ module.exports = {
             const command = client.commands.get(interaction.commandName);
             if (!command) return;
 
-            if (command.botOwnerOnly) {
-                if (!client.config.owners.includes(interaction.user.id)) return interaction.reply({
-                    content: `❌ **Vous devez être le propriétaire du bot pour exécuter cette commande.**`,
-                    flags: ["Ephemeral"]
-                });
-            };
+            const gsRoles = [...new Set(client.config.roles.map(r => r.gs_role))];
 
-            if (command.guildOwnerOnly) {
-                if (interaction.member.guild.ownerId != interaction.user.id && !client.config.owners.includes(interaction.user.id)) return interaction.reply({
-                    content: `❌ **Vous devez être le propriétaire du serveur pour exécuter cette commande.**`,
-                    flags: ["Ephemeral"]
-                });
-            };
+            const hasRole = gsRoles.some(roleId => interaction.member.roles.cache.has(roleId));
 
-            if (command.permissions?.length) {
-                const authorPerms = interaction.channel.permissionsFor(interaction.user) || interaction.member.permissions;
-                if (!authorPerms.has(command.permissions) && !client.config.owners.includes(interaction.user.id)) return interaction.reply({
-                    content: `❌ **Vous n'avez pas les permissions nécessaires pour exécuter cette commande.**`,
-                    flags: ["Ephemeral"]
-                });
-            };
+            if (!hasRole) 
+                return interaction.reply({ content: "Tu n'as pas les permissions necessaires pour utiliser cette commande.", flags: 64 });
 
             command.executeSlash(client, interaction);
             console.log("[CMD-S]", interaction.guild ? `${interaction.guild.name} | ${interaction.channel.name}` : `DM`, `| ${interaction.user.username} | ${command.name}`);
